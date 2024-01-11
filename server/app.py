8802f369ca68f2b518f072ff459ab9df5caba6f1
jim <jim.otieno@student.moringaschool.com>
2024-01-11 20:50:18 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
@@ -20,19 +22,66 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakery_list = [bakery.to_dict() for bakery in bakeries]
+
+    response = make_response(
+        jsonify(bakery_list),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        bakery_dict = bakery.to_dict()
+        response = make_response(
+            jsonify(bakery_dict),
+            200
+        )
+    else:
+        response = make_response(
+            jsonify({"error": "Bakery not found"}),
+            404
+        )
+
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [baked_good.to_dict() for baked_good in baked_goods]
+
+    response = make_response(
+        jsonify(baked_goods_list),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        most_expensive_dict = most_expensive.to_dict()
+        response = make_response(
+            jsonify(most_expensive_dict),
+            200
+        )
+    else:
+        response = make_response(
+            jsonify({"error": "No baked goods found"}),
+            404
+        )
+
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
